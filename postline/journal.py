de8047b705b141a009a48fa28e583b57c9f6b8f4
Wei Dong <wdong@aaalgo.com>
2025-01-19 23:57:06 -0500

    update

--- postline/journal.py
+++ postline/journal.py
@@ -2,28 +2,26 @@ import sys
 import pickle
 def play_journal(self_address, journal):
     output = []
-    with open('a.pkl', 'wb') as f:
-        pickle.dump(journal, f)
-        for entry in journal:
-            message = entry.message
-            if (message["From"] == self_address) and (message["To"] == self_address):
-                subject = message.get("Subject", "")
-                if subject.startswith("REPLACE X-Serial"):
-                    pickle.dump(output, f)
-
-                    _, range_str = subject.split("X-Serial ")
-                    start, end = map(int, range_str.split("-"))
-                    top = output[:start]
-                    bottom = output[end:]
-                    output = []
-                    output.extend(top)
-                    entry.message["Subject"] = "%d journal entries compressed" % (end - start)
-                    output.append(entry)
-                    output.extend(bottom)
-            # Potentially other journal instructions can be processed here
-            else:
+    for entry in journal:
+        message = entry.message
+        if (message["From"] == self_address) and (message["To"] == self_address):
+            subject = message.get("Subject", "")
+            if subject.startswith("REPLACE X-Serial"):
+                _, range_str = subject.split("X-Serial ")
+                start, end = map(int, range_str.split("-"))
+                print(start, end)
+                top = [a for a in output[:start]]
+                bottom = [a for a in output[end:]]
+                output = []
+                output.extend(top)
+                entry.message.replace_header("Subject", "%d journal entries compressed" % (end - start))
                 output.append(entry)
-        pickle.dump(output, f)
-        sys.exit(0)
+                output.extend(bottom)
+                entry.message.replace_header("Subject", "memory compression done %d-%d" % (start, end))
+                entry.message.replace_content("")
+                output.append(entry)
+        # Potentially other journal instructions can be processed here
+        else:
+            output.append(entry)
     return output
 
